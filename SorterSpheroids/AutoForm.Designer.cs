a9f617641da7f81cd9d60be6ee1a76f86c95b9f0
Heis97 <46742591+Heis97@users.noreply.github.com>
2025-04-08 11:32:01 +0300

    0804

--- SorterSpheroids/AutoForm.Designer.cs
+++ SorterSpheroids/AutoForm.Designer.cs
@@ -31,9 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoForm));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.but_scan_cell = new System.Windows.Forms.Button();
+            this.but_find_spheroids = new System.Windows.Forms.Button();
+            this.button_sort_spheroids = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -43,6 +43,7 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,23 +59,24 @@
             this.button2.Name = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // but_scan_cell
             // 
-            resources.ApplyResources(this.button3, "button3");
-            this.button3.Name = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.but_scan_cell, "but_scan_cell");
+            this.but_scan_cell.Name = "but_scan_cell";
+            this.but_scan_cell.UseVisualStyleBackColor = true;
+            this.but_scan_cell.Click += new System.EventHandler(this.but_scan_cell_Click);
             // 
-            // button4
+            // but_find_spheroids
             // 
-            resources.ApplyResources(this.button4, "button4");
-            this.button4.Name = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.but_find_spheroids, "but_find_spheroids");
+            this.but_find_spheroids.Name = "but_find_spheroids";
+            this.but_find_spheroids.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // button_sort_spheroids
             // 
-            resources.ApplyResources(this.button5, "button5");
-            this.button5.Name = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.button_sort_spheroids, "button_sort_spheroids");
+            this.button_sort_spheroids.Name = "button_sort_spheroids";
+            this.button_sort_spheroids.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -118,15 +120,17 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.but_find_spheroids);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.but_scan_cell);
             this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button_sort_spheroids);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.textBox2);
@@ -134,6 +138,13 @@
             resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
             // 
             // AutoForm
             // 
@@ -152,9 +163,9 @@
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button but_scan_cell;
+        private System.Windows.Forms.Button but_find_spheroids;
+        private System.Windows.Forms.Button button_sort_spheroids;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
@@ -164,5 +175,6 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
--- SorterSpheroids/AutoForm.cs
+++ SorterSpheroids/AutoForm.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace SorterSpheroids
 {
+    
     public partial class AutoForm : Form
     {
         MainForm mainForm;
@@ -17,6 +20,129 @@ namespace SorterSpheroids
         {
             InitializeComponent();
             this.mainForm = mainForm;
+            gen_buts_cells();
+        }
+        void gen_buts_cells()
+        {
+            var ps = new List<Point>();
+            var ps_st = new Point(10,30);
+            var dx = 80;
+            var dy = 80;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    gen_button_cells(new Point(ps_st.X + dx * i, ps_st.Y + dy * j),i+" "+j);
+                }
+            }
+
+            groupBox2.SendToBack();
+        }
+        Button gen_button_cells(Point point, string acc_name)
+        {
+            var but1 = new CircularButton(new Size(60,60));
+            but1.Location = point;
+            but1.Text = "";
+            but1.AccessibleName = acc_name;
+            but1.Click += but_choose_cell_Click;
+            groupBox2.Controls.Add(but1);
+
+            return but1;
+        }
+
+        private void but_scan_cell_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void but_choose_cell_Click(object sender, EventArgs e)
+        {
+            var but = (Button)sender;
+            Console.WriteLine(but.AccessibleName);
+        }
+    }
+
+    public class CircularButton : Button
+    {
+        private bool isMouseOver = false;
+
+        public CircularButton(Size size)
+        {
+            // Set default size
+            this.Size = size;
+            // Make the button flat
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+
+            // Subscribe to mouse events
+            this.MouseEnter += CircularButton_MouseEnter;
+            this.MouseLeave += CircularButton_MouseLeave;
+
+            // Set double buffering to reduce flickering
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                         ControlStyles.AllPaintingInWmPaint |
+                         ControlStyles.UserPaint, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            // Clear the background
+            pevent.Graphics.Clear(this.BackColor);
+
+            // Create circular path
+            GraphicsPath graphicsPath = new GraphicsPath();
+            Rectangle bounds = new Rectangle(0,0, this.Width - 1, this.Height - 1 );
+            graphicsPath.AddEllipse(bounds);
+            this.Region = new Region(graphicsPath);
+
+            // Draw the button background
+            Pen pen1 = new Pen(Color.Black, 2);
+            using (SolidBrush brush = new SolidBrush(Color.AliceBlue))
+            {
+                pevent.Graphics.FillEllipse(brush, bounds);
+            }
+
+            // Draw text
+            TextRenderer.DrawText(pevent.Graphics, this.Text, this.Font,
+                bounds, this.ForeColor,
+                TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+
+            // Draw black border when mouse is over
+            if (isMouseOver)
+            {
+                using (Pen pen = new Pen(Color.Blue, 4))
+                {
+                    pen.LineJoin = LineJoin.Round;
+                    pevent.Graphics.DrawEllipse(pen, bounds);
+                }
+            }
+
+
+           // base.OnPaint(pevent);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (this.Width != this.Height)
+            {
+                this.Height = this.Width;
+            }
+            this.Invalidate(); // Redraw on resize
+        }
+
+        private void CircularButton_MouseEnter(object sender, EventArgs e)
+        {
+            isMouseOver = true;
+            this.Invalidate();
+        }
+
+        private void CircularButton_MouseLeave(object sender, EventArgs e)
+        {
+            isMouseOver = false;
+            this.Invalidate();
         }
     }
 }
